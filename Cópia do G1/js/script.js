354445c1d2d992cdcc3c58dde09d60eb4287d7ef
Gustavo <gustavogomes1108@gmail.com>
2025-11-02 01:58:52 -0300

    .

---
+++ Cópia do G1/js/script.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("graficoEconomia").getContext("2d");
+
+const dados = {
+  labels: ["11h", "12h", "13h", "14h", "15h", "16h", "17h"],
+  datasets: [
+    {
+      label: "IBOVESPA",
+      data: [147.192, 147.05, 146.85, 149.7, 149.55, 146.4, 146.25],
+      borderColor: "rgba(20, 197, 94, 0.81)",
+      backgroundColor: "rgba(20, 197, 94, 0.81)",
+      fill: true,
+    },
+  ],
+};
+
+const tabela = new Chart(canvas, {
+  type: "line",
+  data: dados,
+  options: {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "IBOVESPA",
+      },
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
